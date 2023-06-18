020dddc59d644a5d2a2b4d272450952ebd520908
Dilan Salinda <contact.dilansalinda@gmail.com>
2023-06-18 06:55:55 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -7,13 +7,11 @@ smtp_server = "smtp.gmail.com"
 USERNAME = os.environ.get('MAIL_USERNAME')
 PASSWORD = os.environ.get('MAIL_PASSWORD')
 
-print(USERNAME)
-print(PASSWORD)
 
 server = smtplib.SMTP_SSL(smtp_server, port)
-server.login(USERNAME,PASSWORD)
+server.login("myrockemail@gmail.com","pvbrhwpdcikrvale")
 server.sendmail(
-  USERNAME, 
+  "myrockemail@gmail.com", 
   "kubesonar@gmail.com", 
   "this message is from python")
 server.quit()
