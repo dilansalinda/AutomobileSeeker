0c3e038bbfe48b65596e057afd5714ee2da58366
dilansalinda <dilan2012salinda@gmail.com>
2023-06-18 07:22:36 +0530

    fix issue in send_email.py

--- send_email.py
+++ send_email.py
@@ -12,9 +12,9 @@ PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 msg = MIMEMultipart()
 part = MIMEBase('application', "octet-stream")
-part.set_payload(open("file://data.md", "rb").read())
+part.set_payload(open("data.md", "rb").read())
 encoders.encode_base64(part)
-part.add_header('Content-Disposition', 'attachment; filename="file://data.md"')
+part.add_header('Content-Disposition', 'attachment; filename="data.md"')
 msg.attach(part)
 
 server = smtplib.SMTP_SSL(smtp_server, port)
