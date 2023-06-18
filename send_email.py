e91dd068f3902345982630ff74a18d7d35811e13
dilansalinda <dilan2012salinda@gmail.com>
2023-06-18 07:10:04 +0530

    added file attachment to mail

--- send_email.py
+++ send_email.py
@@ -1,3 +1,6 @@
+from email.mime.multipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email import Encoders
 import smtplib 
 import os
 
@@ -7,11 +10,17 @@ smtp_server = "smtp.gmail.com"
 USERNAME = os.environ.get('MAIL_USERNAME')
 PASSWORD = os.environ.get('MAIL_PASSWORD')
 
+msg = MIMEMultipart()
+part = MIMEBase('application', "octet-stream")
+part.set_payload(open("file://data.md", "rb").read())
+Encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="file://data.md"')
+msg.attach(part)
 
 server = smtplib.SMTP_SSL(smtp_server, port)
 server.login("myrockemail@gmail.com","pvbrhwpdcikrvale")
 server.sendmail(
   "myrockemail@gmail.com", 
   "kubesonar@gmail.com", 
-  "this message is from python")
+  msg.as_string())
 server.quit()
